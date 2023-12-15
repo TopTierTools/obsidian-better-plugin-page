f73addb79b9e5c3f26d4c6a2fb241d235e0d71bf
Hananoshika Yomaru <brenkysbwim@gmail.com>
2023-12-14 17:40:27 -0800

    release: 1.0.5

--- src/getName.ts
+++ src/getName.ts
@@ -1,10 +1,8 @@
 import $ from "jquery";
 
 export const getName = (pluginCard: HTMLElement) => {
-	return $(pluginCard)
-		.find(".community-item-name")
-		.contents()
-		.text()
-		.replace("Installed", "")
-		.trim();
+	const name = $(pluginCard).find(".community-item-name").contents().text();
+	// if end with "Installed" remove it
+	const prunedText = name.endsWith("Installed") ? name.slice(0, -9) : name;
+	return prunedText.trim();
 };
